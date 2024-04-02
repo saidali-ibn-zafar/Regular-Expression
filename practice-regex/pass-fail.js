2a406ef10cb80d602461d454d664289a6dd6d59a
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2024-04-02 18:25:02 -0300

    regex | using test() , replace() and replaceAll()

--- practice-regex/pass-fail.js
+++ practice-regex/pass-fail.js
@@ -1,3 +1,5 @@
+// Using test()
+
 function checkElement(arr) {
   let regEx = /(^[a-zA-Z]+([a-zA-Z0-9_]+)?$){1,15}/g;
   for (let i = 0; i < arr.length; i++) {
@@ -11,7 +13,7 @@ function checkElement(arr) {
 }
 
 let inputArray = [
-  "user_123",
+  "user_123", 
   "2user",
   "username",
   "User1",
@@ -30,3 +32,47 @@ let inputArray = [
 
 let result = checkElement(inputArray);
 console.log(result);
+
+- - - - 
+
+// Using replace()
+  
+function checkSingleElement(str) {
+  let regEx = /(^[a-zA-Z]+([a-zA-Z0-9_]+)?$){1,15}/g;
+  let result = str.replace(regEx, "PASS");
+  return result;
+}
+
+let res = checkSingleElement("user_123");
+console.log(res); // PASS
+
+- - - - -
+
+// Using replaceALl()
+  
+function checkAll(str) {
+  let regEx = /(^[a-zA-Z]+([a-zA-Z0-9_]+)?$){1,15}/gm;
+  let result = str.replaceAll(regEx, "PASS");
+  return result;
+}
+
+let strings = `user_123
+2user
+username
+User1
+u
+u#ser
+user
+name
+user!123
+user_name
+a123456789012345
+1234567890123456
+user
+-username
+Username
+user*name
+`;
+
+let res = checkAll(strings);
+console.log(res);
